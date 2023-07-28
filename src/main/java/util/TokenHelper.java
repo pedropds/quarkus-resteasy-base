80dc54cc2d19b3cce02d6df75bb938aba2fac94f
pedropds <pedropintodsousa@gmail.com>
2023-07-28 22:48:42 +0100

    Small changes regarding environmental variables

--- src/main/java/util/TokenHelper.java
+++ src/main/java/util/TokenHelper.java
@@ -3,19 +3,26 @@ package util;
 import dto.UserDTO;
 import io.smallrye.jwt.build.Jwt;
 import jakarta.inject.Singleton;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.HashSet;
 
 @Singleton
 public class TokenHelper {
-    public String generateToken(UserDTO user){
-        long accessTokenExpiration = 15 * 60;
+    @ConfigProperty(name = "mp.jwt.verify.issuer")
+    private String issuer;
 
+    @ConfigProperty(name = "quarkus-resteasy-base.token.expiration")
+    private Long accessTokenExpiration;
+
+    public String generateToken(UserDTO user){
         var roles = new HashSet<>(user.getRoles());
-        return Jwt.issuer("some-issuer")
+        String name = "name";
+
+        return Jwt.issuer(issuer)
                 .upn(user.email)
                 .groups(roles)
-                .claim("name", user.name)
+                .claim(name, user.name)
                 .expiresAt(currentTimeInSecs() + accessTokenExpiration)
                 .sign();
     }
